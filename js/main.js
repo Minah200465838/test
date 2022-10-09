3542f07a471f47fc85ddcc6c1edc32450c3defb3
Minah Cho <19972001@gmail.com>
2022-10-09 02:38:53 -0400

    test

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const carouselBurger = new bootstrap.Carousel('#carouselBurger')
+const carouselText = document.getElementById('carousel-text')
+
+carouselBurger.addEventListener('slide.bs.carousel', event => {
+  // do something...
+})
